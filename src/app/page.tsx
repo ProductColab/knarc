a66d686b21d662f6a9647258b2a4e4695340ec2b
Nic Galluzzo <nic@galluzzo.us>
2024-11-19 09:08:09 -0800

    minor cleaup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,5 @@ import { ConfigContent } from "@/features/config/components/ConfigContent";
 export const runtime = "edge";
 
 export default function Home() {
-  // Show wizard if no settings, otherwise redirect to first application
   return <ConfigContent configId={null} />;
 }
